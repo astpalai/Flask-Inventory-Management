a104bd8f92142c81a33d2bea832c0bb6e9f74b47
astpalai <astpalai@gmail.com>
2023-04-27 12:52:44 +0300

    Created base layout and home page

--- website/__init__.py
+++ website/__init__.py
@@ -3,5 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev'
+
+    from website.views import views
+    app.register_blueprint(views, url_prefix='/')
     
     return app
