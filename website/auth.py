e2226a8711c27bcccde2298cf7e733770326caad
astpalai <astpalai@gmail.com>
2023-04-28 19:12:26 +0300

    Converted registration form to wtf Flaskform

--- website/auth.py
+++ website/auth.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from website import db
 from website.models import User
+from website.forms import RegisterForm
 
 auth = Blueprint('auth', __name__)
 
@@ -32,40 +33,21 @@ def logout():
     logout_user()
     return redirect(url_for('views.home'))
 
-@auth.route('/register', methods = ['GET', 'POST'])
+@auth.route('register', methods = ['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')        
-        password1 = request.form.get('password')
-        password2 = request.form.get('password_confirm')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email = form.email.data,
+                     password = generate_password_hash(form.password.data, method='scrypt'))
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        flash('Account created successfully.', category='success')
+        
+        return redirect(url_for('views.home'))
 
-        # Validations
-        user_mail = User.query.filter_by(email = email).first()
-        user_name = User.query.filter_by(username = username).first()
-        create_db = True
-        if user_mail:
-            flash('Email already exists.', category = 'danger')
-            create_db = False
-        if user_name:
-            flash('Username already exists.', category = 'danger')
-            create_db = False
-        if len(username)<2:
-            flash('Username must be longer than one character', category = 'danger')
-            create_db = False
-        if len(password1)<2:
-            flash('Password must be longer than one character', category = 'danger')
-            create_db = False
-        if password1 != password2:
-            flash('Passwords don\'t match.', category='danger')
-            create_db = False
-        if create_db == True:
-            new_user = User(email = email, username = username, password = generate_password_hash(
-                password1, method = 'scrypt'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash('Account created!', category = 'success')
-            return redirect(url_for('views.home'))    
-
-    return render_template('register.html')
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(*err_msg, category='danger')
+    
+    return render_template('register.html', form=form)
