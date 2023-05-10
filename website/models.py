6b85fc6e4cf35f35445c3ec26c1cf5656ec40c80
astpalai <astpalai@gmail.com>
2023-05-10 09:47:10 +0300

    Added budget to users

--- website/models.py
+++ website/models.py
@@ -6,6 +6,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), unique = True, nullable = False)
     email = db.Column(db.String(50), unique = True, nullable = False)
     password = db.Column(db.String(20))
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
 
 class Products(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
